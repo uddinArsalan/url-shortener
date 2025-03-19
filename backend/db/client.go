4a48aba5ec6248488bfe6ecf19e32ba41a7e4eee
Arsalan <uddinarsalan91@gmail.com>
2025-03-20 02:38:42 +0530

    added db calls and change db from mysql to postgres and some fixes

--- backend/db/client.go
+++ backend/db/client.go
@@ -3,51 +3,59 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"github.com/bwmarrin/snowflake"
-	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/joho/godotenv/autoload"
 	"log"
 	"os"
 	"time"
+
+	"github.com/bwmarrin/snowflake"
+	// _ "github.com/go-sql-driver/mysql"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
 )
 
-type UserService struct {
-	db *sql.DB
+var db *sql.DB
+
+type User struct {
+	ID           int64
+	Username     string
+	Email        string
+	PasswordHash string
+	CreatedAt    string
 }
 
-func NewUserService(db *sql.DB) UserService {
-	return UserService{db: db}
+type URL struct {
+	ID int64
+	OriginalUrl string
+	ShortCode string
+	UserId int64
+	CreatedAt string
+	Clicks int32
 }
 
-func NewClient() {
-	DB_NAME := os.Getenv("DB_NAME")
-	DB_HOST := os.Getenv("DB_HOST")
-	DB_USERNAME := os.Getenv("DB_USERNAME")
-	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+func InitDBClient() {
+	DATABASE_NAME := os.Getenv("DATABASE_NAME")
+	DATABASE_HOST := os.Getenv("DATABASE_HOST")
+	DATABASE_USER := os.Getenv("DATABASE_USER")
+	DATABASE_PASSWORD := os.Getenv("DATABASE_PASSWORD")
 
-	dbUri := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)
+	connStr := fmt.Sprintf("user='%s' password=%s host=%s dbname=%s", DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME)
 
-	db, err := sql.Open("mysql", dbUri)
+	// dbUri := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)
 
-	if err != nil {
-		log.Fatalf("Error opening database: %v", err)
-	}
+	db, _ = sql.Open("postgres", connStr)
 
 	if err := db.Ping(); err != nil {
 		db.Close()
 		log.Fatalf("Database connection failed: %v", err)
 	}
-
 	fmt.Println("Database connected successfully!")
 
-	service := NewUserService(db)
-	fmt.Printf("Service is ready! %v\n", service)
-
-	createUserTable(service.db)
-	InsertUser(User{"Arsu", "arsalan91@gmail.com", "random_password"}, service.db)
+	CreateUserTable()
+	CreateUrlTable()
+	CreateAnalyticsTable()
 }
 
-func createUserTable(db *sql.DB) {
+func CreateUserTable() {
 	query := `
 	CREATE TABLE IF NOT EXISTS users(
 	id BIGINT PRIMARY KEY ,
@@ -67,25 +75,125 @@ func createUserTable(db *sql.DB) {
 
 }
 
-type User struct {
-	username      string
-	email         string
-	password_hash string
+func CreateUrlTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS urls(
+	id BIGINT PRIMARY KEY,
+	original_url TEXT NOT NULL,
+	shortcode VARCHAR(7) UNIQUE NOT NULL,
+	user_id BIGINT references users(id),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    clicks INT DEFAULT 0
+	)
+	`
+	res, err := db.Exec(query)
+	if err == nil {
+		fmt.Println("Url Table Created ", res)
+	} else {
+		log.Fatalf("Error creating Url table %v", err)
+	}
 }
 
-func InsertUser(user User, db *sql.DB) {
+func CreateAnalyticsTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS analytics(
+	id BIGINT PRIMARY KEY,
+	url_id BIGINT REFERENCES urls(id), 
+	ip_address VARCHAR(45),
+    user_agent TEXT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)
+	`
+	res, err := db.Exec(query)
+	if err == nil {
+		fmt.Println("Analytics Table Created ", res)
+	} else {
+		log.Fatalf("Error creating Analytics table %v", err)
+	}
+}
+
+func FindUrlFromShortCode(shortCode string) (string, error) {
+	query := `SELECT original_url FROM urls WHERE shortcode = $1`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return "", err
+	}
+	defer stmt.Close()
+	var originalURL string
+	er := stmt.QueryRow(shortCode).Scan(&originalURL)
+	if er != nil {
+		fmt.Printf("Error querying URL with given short code %v\n", err)
+		if er == sql.ErrNoRows {
+			return "", fmt.Errorf("no URL found for the given short code: %v", shortCode)
+		}
+		return "", err
+	}
+	return originalURL, nil
+}
+
+func InsertUser(user User) {
 	node, err := snowflake.NewNode(1)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	id := node.Generate()
-	query := `INSERT INTO users (id,username, email,password_hash, created_at) VALUES (?, ?, ?,?,?)`
-	result, err := db.Exec(query, id, user.username, user.email, user.password_hash, time.Now())
+	query := `INSERT INTO users (id,username, email,password_hash, created_at) VALUES ($1, $2, $3, $4, $5)`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(id.Int64(), user.Username, user.Email, user.PasswordHash, time.Now())
 	if err != nil {
 		log.Fatalf("Error inserting data in users table %v", err)
 	} else {
 		fmt.Println("User inserted successfully!")
 	}
-	fmt.Println(result.LastInsertId())
+	fmt.Println(result)
+}
+
+func FindUserByEmail(email string) (User, error) {
+	query := `SELECT id, username, email, created_at FROM users WHERE email = $1`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return User{}, fmt.Errorf("error preparing query: %w", err)
+	}
+	defer stmt.Close()
+	var user User
+
+	err = stmt.QueryRow(email).Scan(&user.ID, &user.Username, &user.Email, &user.CreatedAt)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return User{}, fmt.Errorf("no user found with email: %s", email)
+		}
+		return User{}, fmt.Errorf("error querying user: %w", err)
+	}
+	return user, nil
+}
+
+func InsertUrl(url URL) {
+	node, err := snowflake.NewNode(1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	id := node.Generate()
+	// 1902446527184375808
+	query := `INSERT INTO urls (id,original_url,shortcode,user_id, created_at,clicks) VALUES ($1, $2, $3, $4, $5, $6)`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(id.Int64(), url.OriginalUrl, url.ShortCode, url.UserId, time.Now(),url.Clicks)
+	if err != nil {
+		log.Fatalf("Error inserting data in urls table %v", err)
+	} else {
+		fmt.Println("Urls inserted successfully!")
+	}
+	fmt.Println(result)
 }
--- backend/main.go
+++ backend/main.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/redis/go-redis/v9"
+	"github.com/rs/cors"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 	"url_shortener/db"
-	"github.com/rs/cors"
 )
 
 // var urlMap = make(map[string]string)
@@ -39,8 +39,14 @@ func shorten(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to save URL", http.StatusInternalServerError)
 		return
 	}
+	url := db.URL{
+		OriginalUrl: userUrl,
+		ShortCode:   shortCode,
+		UserId:      1902446527184375808,
+	}
+	db.InsertUrl(url)
 	fmt.Printf("\nUrl shortened from %s to %s", userUrl, shortCode)
-	w.Write([]byte(fmt.Sprintf("http://localhost/url/%s", shortCode)))
+	w.Write(fmt.Appendf(nil, "http://localhost/url/%s", shortCode))
 }
 func redirect(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -71,17 +77,33 @@ func getAllUrls(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	db.InitDBClient()
 	if err := db.InitRedis(); err != nil {
 		log.Fatalf("Failed to initialize Redis: %v", err)
 	}
 	r := mux.NewRouter()
-
 
 	r.HandleFunc("/shorten", shorten).Methods("POST")
 
 	r.HandleFunc("/url/{shortCode}", redirect).Methods("GET")
 
 	r.HandleFunc("/get", getAllUrls).Methods("GET")
+	r.HandleFunc("/createUser", func(w http.ResponseWriter, r *http.Request) {
+		user := db.User{
+			Username:     "Arsu",
+			Email:        "arsu91@gmail.com",
+			PasswordHash: "12364643",
+		}
+		db.InsertUser(user)
+	}).Methods("POST")
+	r.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		email := r.URL.Query().Get("email")
+		user, err := db.FindUserByEmail(email)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(user)
+	})
 
 	handler := cors.Default().Handler(r)
 
