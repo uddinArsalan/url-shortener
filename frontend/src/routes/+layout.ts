1e704e7fd8e287685b8e72484ec7c86d23e49a53
Arsalan <uddinarsalan91@gmail.com>
2025-09-19 20:13:06 +0530

    Fix indefinite loading of frontend

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,32 +1,14 @@
-import type { LayoutLoad } from "./$types";
+import { onMount } from "svelte";
 import { fetchUser } from "$lib/api/auth";
 import { userStore } from "$lib/store/userStore";
 // import { redirect } from "@sveltejs/kit";
 // import { API_BASE_URL } from "../constants";
 
-export const load: LayoutLoad = async () => {
+onMount(async () => {
   try {
     const userDetails = await fetchUser();
-    userStore.update((store) => {
-      store.user = userDetails;
-      store.isLoggedIn = true;
-      // store.isLoading = false;
-      return store;
-    });
-  } catch (error) {
-    // console.log("Failed to fetch user details:", error);
-    userStore.update((store) => {
-      store.user = {
-        id: 0,
-        email: "",
-        username: "",
-        created_at: "",
-      };
-      store.isLoggedIn = false;
-      // store.isLoading = false;
-      return store;
-    });
-    // redirect(307, `${API_BASE_URL}/auth/login`);
+    userStore.update((s) => ({ ...s, user: userDetails, isLoggedIn: true }));
+  } catch (err) {
+    userStore.update((s) => ({ ...s, user: null, isLoggedIn: false }));
   }
-  return {};
-};
+});
